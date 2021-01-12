12a4cb7f81ad0b398a6c5cd2a729b85c54cc64f1
Alessio Franceschi <alessiofranceschi2@gmail.com>
2021-01-12 11:28:58 +0100

    Added basic metadata in head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { Fragment } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<Head>
+				<title>React Commenting System</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
