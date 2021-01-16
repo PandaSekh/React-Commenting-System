2b0a1df74a94f6fe5dce17e40833d37663cb6b86
Alessio Franceschi <alessiofranceschi2@gmail.com>
2021-01-16 12:12:12 +0100

    Added URL to Repo

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
 			<h1>React (NextJS) / Sanity Commenting System</h1>
 			<h2>
 				Designed and Programmed by{" "}
-				<a href="https://github.com/PandaSekh/">Alessio Franceschi</a>
+				<a href="https://github.com/PandaSekh/React-Commenting-System">Alessio Franceschi</a>
 			</h2>
 		</Fragment>
 	);
